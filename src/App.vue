fbc92d7aff12104d3ccc3f23bd1047a18d07e563
SakinalaRaj <143786644+SakinalaRaj@users.noreply.github.com>
2023-10-11 17:41:16 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div>Parent Component count value: {{ count }}</div>
+    <div>
+      <ChildViewVue
+        :increamentCount="increamentCount"
+        :decreamentCount="decreamentCount"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ChildViewVue from "./components/ChildView.vue";
+const count = ref(0);
+const decreamentCount = () => {
+  count.value--;
+};
+const increamentCount = (a) => {
+  console.log("a", a);
+  count.value++;
+};
+</script>
+<style>
+#app {
+  text-align: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/ChildView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Child component</div>
+  <div>
+    <button @click="props.decreamentCount(2)">-</button>
+    <button @click="props.increamentCount(3)">+</button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps(["increamentCount", "decreamentCount"]);
+</script>
+
+<style lang="scss" scoped></style>
